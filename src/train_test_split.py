6ecb68e1c9b7cbc6cce157b4fb6df6132154cf00
Hamza <139881206+HmxaaK@users.noreply.github.com>
2025-08-14 13:59:57 +0500

    Delete src/train_test_split.py

--- src/train_test_split.py
+++
@@ -1,6 +0,0 @@
-def train_test_split(features,target):
-  train_size = int(len(features)*0.60)
-  X_train,y_train=features.iloc[:train_size,:],target.iloc[:train_size]
-  X_test,y_test=features.iloc[train_size:,:],target.iloc[train_size:]
-  return X_train,y_train,X_test,y_test
-    
