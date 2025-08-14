697ef409c4d0719b14d7783a51ed5ea99950d98e
Hamza <139881206+HmxaaK@users.noreply.github.com>
2025-08-14 13:58:32 +0500

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -25,4 +25,10 @@ def feature_engineering(data_set):
   y = data[target]
   y = y.loc[X.index]
   return X,y
-  
+
+def train_test_split(features,target):
+  train_size = int(len(features)*0.60)
+  X_train,y_train=features.iloc[:train_size,:],target.iloc[:train_size]
+  X_test,y_test=features.iloc[train_size:,:],target.iloc[train_size:]
+  return X_train,y_train,X_test,y_test
+
