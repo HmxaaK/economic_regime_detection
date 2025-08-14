ec0fc0186f835e219d440e07f7b3348425942050
Hamza <139881206+HmxaaK@users.noreply.github.com>
2025-08-14 14:05:13 +0500

    Create run_model.py

---
+++ src/run_model.py
@@ -0,0 +1,13 @@
+data = pd.read_csv('Recession Indicators(Sheet1).csv',index_col='Date',
+                   parse_dates=True).dropna()
+X,y = feature_engineering(data)
+X_train,y_train,X_test,y_test = train_test_split(X,y)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+best_params = hyperparameter_tuning(X_train_scaled,y_train)
+model = LogisticRegression(**best_params).fit(X_train_scaled,y_train)
+X_test_scaled = scaler.transform(X_test)
+logit_preds = model.predict(X_test_scaled)
+logit_probs = model.predict_proba(X_test_scaled)
+model_performance(y_test,logit_preds)
+temporal_mapping(logit_probs, y_test)
