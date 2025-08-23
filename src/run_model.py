4135fdb852484e70f83e347f0ec32bba61fdfa60
Hamza <139881206+HmxaaK@users.noreply.github.com>
2025-08-23 14:54:09 +0500

    Update run_model.py

--- src/run_model.py
+++ src/run_model.py
@@ -10,7 +10,7 @@ from sklearn.metrics import confusion_matrix,ConfusionMatrixDisplay
 import plotly.express as px
 from preprocessing import feature_engineering,train_test_split
 from hyperparameter_tuning import hyperparameter_tuning
-from evaluation import model_performance,temporal_mapping
+from evaluation import model_performance
 
 data = pd.read_csv('data/Recession Indicators(Sheet1).csv',index_col='Date',
                    parse_dates=True).dropna()
