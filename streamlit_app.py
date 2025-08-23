31595067d743438642c4c5fbb3475f2e1f9f7966
Hamza <139881206+HmxaaK@users.noreply.github.com>
2025-08-23 16:20:59 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,32 @@
 import streamlit as st
 from back_testing.py import temporal_mapping
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_score,recall_score,precision_recall_curve
+from sklearn.metrics import f1_score,make_scorer
+from sklearn.model_selection import TimeSeriesSplit, GridSearchCV
+from sklearn.metrics import confusion_matrix,ConfusionMatrixDisplay
+import plotly.express as px
+from src.preprocessing import feature_engineering,train_test_split
+from src.hyperparameter_tuning import hyperparameter_tuning
+from src,evaluation import model_performance
+
+data = pd.read_csv('data/Recession Indicators(Sheet1).csv',index_col='Date',
+                   parse_dates=True).dropna()
+X,y = feature_engineering(data)
+X_train,y_train,X_test,y_test = train_test_split(X,y)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+best_params = hyperparameter_tuning(X_train_scaled,y_train)
+model = LogisticRegression(**best_params).fit(X_train_scaled,y_train)
+X_test_scaled = scaler.transform(X_test)
+logit_preds = model.predict(X_test_scaled)
+logit_probs = model.predict_proba(X_test_scaled)
+
+
 # ---------------- Streamlit UI ----------------
 st.title('Economic Health Index')
 st.subheader('Macroeconomic Indicators for Regime Classification')
@@ -41,4 +68,8 @@ st.subheader('Solution')
 st.write("We use historical monthly macroeconomic indicators and NBER labels to train a logistic regression model. This model can:")
 st.write("- Estimate the probability of being in a recession this month.")
 st.write("- Convert that probability into an Economic Health Score (0–10 scale).")
+
+
+fig1, fig2 = temporal_mapping(logit_probs, y_test)
+st.plotly_chart(fig1, use_container_width=True)
 
