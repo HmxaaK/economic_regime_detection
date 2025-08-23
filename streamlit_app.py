22333fe7046a7a57f72d96f816dceabd5c1603bd
Hamza <139881206+HmxaaK@users.noreply.github.com>
2025-08-23 13:37:51 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,4 @@
 import streamlit as st
 st.title('🎈Economic Health Index')
+st.header('The index is derived by transforming the nowcasted recession probabilities obtained from recession indicators.')
+st.subheader('placeholder')
