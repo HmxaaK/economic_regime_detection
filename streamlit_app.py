b8032998162803f4920f7712af8611740e944b99
Hamza <139881206+HmxaaK@users.noreply.github.com>
2025-08-23 13:45:17 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit as st
 st.title('Economic Health Index')
 st.subheader('Macroeconomic Indicators for Regime Classification')
 st.write('Economic regimes are characterized by dynamic complexity and the movement of variables. Some movements coincide with the economic downturns or rallies, while others show fluctuations during transitions. In order to separate economic regimes at their peak and trough, the choice of these variables is crucial for accurate classification') 
-
+st.subheader('Coincident Indicators')
+st.write('The variables that show significant fluctuations during the economic downturns and expansion are called coincident economic variables. The most significant of these in identifying economic regimes are Payroll employment, Civilian employment, Industrial production, and Unemployment rate.')
+st.subheader('Lagging Indicators')
+st.write('These indicators fluctuate after an economic downturn starts. The unemployment rate is the most significant indicator in this context, as it spikes greatly after the start of the recession and falls when the economic activity reaches its peak. Significant lagging variables include CPI and Bank interest rates, as indicated by (Hiroshi Iyetomi, 2020).')
