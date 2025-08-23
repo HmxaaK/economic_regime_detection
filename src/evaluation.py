bf4265825101da96ebb4c0a213465b9e28afa846
Hamza <139881206+HmxaaK@users.noreply.github.com>
2025-08-23 14:47:50 +0500

    Update evaluation.py

--- src/evaluation.py
+++ src/evaluation.py
@@ -4,26 +4,6 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import streamlit as st
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-
-def temporal_mapping(probs, labels):
-    probs_recession = probs[:,1]
-    plt.figure(figsize=(12, 6))
-    plt.plot(labels.index, probs_recession, label="Probability of Recession")
-    mask = (labels == 1).to_numpy()
-    plt.fill_between(
-        labels.index, 0, probs_recession,
-        where=mask,
-        color='grey', label="NBER = Recession"
-    )
-    plt.ylim(0,1)
-    plt.ylabel("Probability")
-    plt.legend()
-    plt.show()
-
-    economic_health_scores = np.round((probs[-39:, 0] * 10),2)
-    fig = px.line(x=labels.index[-39:],y=economic_health_scores,
-                  labels={"x":"","y":"Economic Health Score"})
-    fig.show()
     
 def model_performance(y_actual,y_predict):
   recall = recall_score(y_actual,y_predict)
