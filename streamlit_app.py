62d95f9eaade7d8e16b50a03334a082adcb3f2b2
Hamza <139881206+HmxaaK@users.noreply.github.com>
2025-08-23 13:42:43 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title('Economic Health Index')
-st.header('Macroeconomic Indicators for Regime Classification')
+st.subheader('Macroeconomic Indicators for Regime Classification')
 st.write('Economic regimes are characterized by dynamic complexity and the movement of variables. Some movements coincide with the economic downturns or rallies, while others show fluctuations during transitions. In order to separate economic regimes at their peak and trough, the choice of these variables is crucial for accurate classification') 
 
